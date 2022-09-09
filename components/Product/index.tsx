6693d0d196bc236f57b183c4a8ea84e1c1e963d3
Lucas Gazzola <lucasgazzola1@outlook.com>
2022-09-09 02:54:10 -0300

    add product item structure

--- components/Product/index.tsx
+++ components/Product/index.tsx
@@ -1,15 +1,29 @@
 import { ProductInterface } from "interfaces";
+import Image from "next/image";
+import Link from "next/link";
 
-function Product({
-  id,
-  category,
-  description,
-  image,
-  price,
-  rating,
-  title
-}: ProductInterface) {
-  return <li></li>;
+import style from "./Product.module.css";
+
+function Product({ id, image, price, rating, title }: ProductInterface) {
+  return (
+    <li className={style.linkContainer}>
+      <Link href={`products/${id.toString()}`}>
+        <a className={style.productContainer}>
+          <header>
+            <h3>{title}</h3>
+          </header>
+          <div className={style.imageContainer}>
+            <Image alt={title} src={image} layout="fill" objectFit="contain" />
+          </div>
+          <footer>
+            <p className={style.descriptionContainer}>
+              ${price} - {rating.rate}⭐
+            </p>
+          </footer>
+        </a>
+      </Link>
+    </li>
+  );
 }
 
 export default Product;
