6013438ef5d80e4d942824678569e1738586f34d
Lucas Gazzola <lucasgazzola1@outlook.com>
2022-09-12 22:08:00 -0300

    add config, docker and deps

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: { domains: ["fakestoreapi.com"] }
+  images: { domains: ["fakestoreapi.com"] },
+  output: "standalone"
 };
 
 module.exports = nextConfig;
