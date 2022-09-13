8922a591edb42b42cb24675fd8fcc7d21eeed2c6
Lucas Gazzola <lucasgazzola1@outlook.com>
2022-09-13 04:53:38 -0300

    single product page style

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,11 +1,16 @@
 import { GetStaticProps, GetStaticPropsContext, NextPage } from "next";
+import Link from "next/link";
+import Image from "next/image";
+
+import { IoMdArrowBack } from "react-icons/io";
+import { GrPrevious, GrNext } from "react-icons/gr";
 
 import { PageLayout } from "layouts";
 import { ProductInterface } from "interfaces";
 
-// TODO: replace db.json
+import style from "styles/[id].module.css";
+
 import data from "db.json";
-import Link from "next/link";
 
 type Props = {
   product: ProductInterface;
@@ -13,28 +18,63 @@ type Props = {
 };
 
 const Product: NextPage<Props> = ({ product, total }: Props) => {
+  const { title, image, rating, price, description, category } = product;
   return (
     <PageLayout title="Shop">
-      <Link href="/products">
-        <a>
-          {/* // TODO: Add GO BACK ICON */}
-          <div>&lt;- BACK</div>
-        </a>
-      </Link>
-      <div>
-        This is a product with id: {product.id} and name: {product.title}
-      </div>
-      <div>
-        {product.id > 1 && (
-          <Link href={`${product.id - 1}`}>
-            <a>&lt; Back</a>
-          </Link>
-        )}
-        {product.id < total && (
-          <Link href={`${product.id + 1}`}>
-            <a>Next -&gt;</a>
-          </Link>
-        )}
+      <div className={style.singleProductPage}>
+        <Link href="/products">
+          <a>
+            <div className={style.backButtonContainer}>
+              <IoMdArrowBack size={24} />
+              <span className={style.textContainer}>BACK</span>
+            </div>
+          </a>
+        </Link>
+        <div className={style.productContainer}>
+          <header className={style.productContainer}>
+            <span className={style.productTitle}>{title}</span>
+            <span className={style.productCategory}>{category}</span>
+          </header>
+          <main className={style.main}>
+            <div className={style.imageContainer}>
+              <Image
+                priority
+                alt={title}
+                src={image}
+                width={350}
+                height={400}
+                objectFit="contain"
+              />
+            </div>
+            <article className={style.descriptionContainer}>
+              <p className={style.description}>{description}</p>
+            </article>
+          </main>
+          <footer>
+            <p>
+              <span className={style.price}>${price}</span> -{" "}
+              <span className={style.rate}>{rating.rate}⭐</span>
+            </p>
+          </footer>
+        </div>
+        <div className={style.navigationContainer}>
+          {product.id > 1 && (
+            <Link href={`${product.id - 1}`}>
+              <a className={style.prevNextButtons}>
+                <GrPrevious />
+                <span>Previous</span>
+              </a>
+            </Link>
+          )}
+          {product.id < total && (
+            <Link href={`${product.id + 1}`}>
+              <a className={style.prevNextButtons}>
+                <span>Next</span>
+                <GrNext />
+              </a>
+            </Link>
+          )}
+        </div>
       </div>
     </PageLayout>
   );
