7b4506d09008865a7529a38b497bbe16f2782e1a
Lucas Gazzola <lucasgazzola1@outlook.com>
2022-09-15 22:23:38 -0300

    prevent error with server

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,27 @@
-import "styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 import { PageLayout } from "layouts";
+import "styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <PageLayout title="Home / App">
-      <Component {...pageProps} />
-    </PageLayout>
-  );
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+
+  if (typeof window === "undefined") {
+    return <></>;
+  } else {
+    return (
+      <PageLayout title="Home / App">
+        <Component {...pageProps} />
+      </PageLayout>
+    );
+  }
 }
 
 export default MyApp;
